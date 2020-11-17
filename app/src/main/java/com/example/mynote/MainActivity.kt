6bd9f47fae2d43ce28bb33a8c5340cb6d2a2f7a0
RP <rp.eskandary@gmail.com>
2020-11-17 11:32:02 +0330

    recycler adapter working

--- app/src/main/java/com/example/mynote/MainActivity.kt
+++ app/src/main/java/com/example/mynote/MainActivity.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,10 +16,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        recycler_main.layoutManager = LinearLayoutManager(this)
+
+        val adapter = NoteAdapter()
+
+        recycler_main.adapter = adapter
+
         mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         mainViewModel.getAllNotes().observe( this, Observer<List<Note>>{
             Log.i("THIS", it.size.toString())
+            adapter.notes = it
+
         })
+
+
+
+
+
+
 
 
     }
--- app/src/main/java/com/example/mynote/NoteAdapter.kt
+++ app/src/main/java/com/example/mynote/NoteAdapter.kt
@@ -3,15 +3,13 @@ package com.example.mynote
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.note_item.view.*
 
 class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteAdapterHolder>() {
 
-
-    private val notes : List<Note?> = ArrayList<Note>()
-
-
-
+     var notes : List<Note?> = ArrayList<Note>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteAdapterHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -20,6 +18,11 @@ class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteAdapterHolder>() {
     }
 
     override fun onBindViewHolder(holder: NoteAdapterHolder, position: Int) {
+//        val currentNote: Note = getItem(position)
+        val currentNote: Note = notes[position]!!
+        holder.txtViewTitle.text = currentNote.title
+        holder.txtViewDescription.text = currentNote.description
+        holder.txtViewPriority.text = currentNote.priority.toString()
 
     }
 
@@ -28,10 +31,14 @@ class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteAdapterHolder>() {
     }
 
 
-    class NoteAdapterHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    inner class NoteAdapterHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        var txtViewTitle: TextView = itemView.txt_title
+        var txtViewPriority: TextView = itemView.txt_priority
+        var txtViewDescription: TextView = itemView.txt_description
 
     }
-
 
 
 }
